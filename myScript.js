400f603ff1fd8d02032810996c13fd8068193fd2
hjpnam <hjpnam@gmail.com>
2017-02-12 14:13:00 -0600

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,16 +1,17 @@
 YUI().use(
-  'aui-scheduler',
-  function(Y) {
+    'aui-scheduler',
+    function(Y) {
     var events = [
-      {
+        {
         content: 'Review Session',
         endDate: new Date(2017, 2, 13, 5),
         startDate: new Date(2017, 2, 13, 3)
+        }
     ];
-
+    var eventRecorder = new Y.SchedulerEventRecorder();
     var agendaView = new Y.SchedulerAgendaView();
     var dayView = new Y.SchedulerDayView();
-    var eventRecorder = new Y.SchedulerEventRecorder();
+  
     var monthView = new Y.SchedulerMonthView();
     var weekView = new Y.SchedulerWeekView();
     
