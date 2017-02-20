7edf663bad9f93efb694070ed13ae9364d9b2570
hjpnam <hjpnam@gmail.com>
2017-02-20 16:37:50 -0600

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,28 +1,30 @@
+
+var events = [];
+
 YUI().use(
     'aui-scheduler',
     function(Y) {
-    var events = [
-
-    ];
-    var eventRecorder = new Y.SchedulerEventRecorder();
-    var dayView = new Y.SchedulerDayView();
-    var agendaView = new Y.SchedulerAgendaView();
-    var monthView = new Y.SchedulerMonthView();	
-    var weekView = new Y.SchedulerWeekView();
+		
+		var eventRecorder = new Y.SchedulerEventRecorder();
+		var dayView = new Y.SchedulerDayView();
+		var agendaView = new Y.SchedulerAgendaView();
+		var monthView = new Y.SchedulerMonthView();	
+		var weekView = new Y.SchedulerWeekView();
     
-    new Y.Scheduler(
-      {
-        activeView: weekView,
-        boundingBox: '#myScheduler',
-        date: new Date(),
-        eventRecorder: eventRecorder,
-        items: events,
-        render: true,
-        views: [dayView, weekView, monthView, agendaView]
-      }
-    );
-  }
-);
+		new Y.Scheduler(
+			{
+				activeView: weekView,
+				boundingBox: '#myScheduler',
+				date: new Date(),
+				eventRecorder: eventRecorder,
+				items: events,
+				render: true,
+				views: [dayView, weekView, monthView, agendaView]
+			}
+		);
+	}
+)
+
 
 bootcards.init( {
   offCanvasBackdrop : true,
